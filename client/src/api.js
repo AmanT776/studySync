0263103a4ff5b1e06b6bdd228e0e2aff8a3f891d
AmanT776 <amantes776@gmail.com>
2025-07-20 17:22:06 +0300

    env setup

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import dotenv from 'dotenv';
 
-const apiUrl = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api'; // fallback to localhost
+const apiUrl = import.meta.env.VITE_API_BASE_URL; // fallback to localhost
 
 const api = axios.create({
     baseURL: apiUrl,
