40346956a6e7b549de24907771178ac8515dfbf1
AmanT776 <amantes776@gmail.com>
2025-07-20 16:58:21 +0300

    fixed api configuration

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import dotenv from 'dotenv';
 
-const apiUrl = import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:5000/api'; // fallback to localhost
+const apiUrl = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api'; // fallback to localhost
 
 const api = axios.create({
     baseURL: apiUrl,
@@ -10,6 +10,6 @@ const api = axios.create({
   }
 });
 
-console.log('API Base URL:', apiUrl);
+console.log(apiUrl)
 
 export default api; 
